fd416ece6cc597291b051f1a01130493e612828a
matthew goodman <mattsgood2@aol.com>
2017-08-16 23:40:46 +0100

    added modeld __str__ to make readable

--- movies/films/models.py
+++ movies/films/models.py
@@ -7,3 +7,6 @@ class Film(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, max_length=100)
     release_year = models.CharField(max_length=10)
+
+    def __str__(self):
+        return ('{} : {}'.format(self.title, self.description))
