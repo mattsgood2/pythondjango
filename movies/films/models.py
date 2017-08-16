5d60f69ebe6a193aa204d8388d436f5859c9d295
matthew goodman <mattsgood2@aol.com>
2017-08-16 23:02:26 +0100

    added models to Film

---
+++ movies/films/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Film(models.Model):
+    created_on = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, max_length=100)
+    release_year = models.CharField(max_length=10)
