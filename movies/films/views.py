06b51fd4530526d1be5bf8b700842f260dc0769b
matthew goodman <mattsgood2@aol.com>
2017-08-17 01:14:32 +0100

    templates and film.html added

--- movies/films/views.py
+++ movies/films/views.py
@@ -6,5 +6,5 @@ from .models import Film
 # Create your views here.
 def film_list(request):
     films = Film.objects.all()
-    output = ','.join([str(film) for film in films])
-    return HttpResponse(output)
+    #output = ','.join([str(film) for film in films])
+    return render(request, 'films/film_list.html', {'films': films})
