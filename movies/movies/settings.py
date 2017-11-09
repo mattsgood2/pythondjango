20c0e8a973d8a4fba1a82be0708bbb10d65e882a
matthew goodman <mattsgood2@aol.com>
2017-11-09 13:04:18 +0000

    removed template in settings base-dir

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -130,8 +130,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,
-                 'assets'
-                 'templates'
-                 ),
+    os.path.join(BASE_DIR, 'assets'),
 )
