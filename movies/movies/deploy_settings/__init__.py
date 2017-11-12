0c16159394fc40618cea35867e6f7bf6dcb55497
matthew goodman <mattsgood2@aol.com>
2017-11-12 22:48:21 +0000

    whitenoise is messing around

--- movies/movies/deploy_settings/__init__.py
+++ movies/movies/deploy_settings/__init__.py
@@ -14,7 +14,3 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
