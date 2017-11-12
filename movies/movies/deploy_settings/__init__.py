5dd983fddb07b832c10031589c8a35df2bba1e22
matthew goodman <mattsgood2@aol.com>
2017-11-12 22:44:45 +0000

    whitenoise to Gzip

--- movies/movies/deploy_settings/__init__.py
+++ movies/movies/deploy_settings/__init__.py
@@ -16,4 +16,5 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
