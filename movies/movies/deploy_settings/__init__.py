5186111ee8cbae59e131a3961e9e973c43120229
matthew goodman <mattsgood2@aol.com>
2017-11-12 22:27:48 +0000

    removed settings whitenoise from deploy settings

--- movies/movies/deploy_settings/__init__.py
+++ movies/movies/deploy_settings/__init__.py
@@ -15,5 +15,5 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
