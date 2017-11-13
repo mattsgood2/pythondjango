d094cd6d250ddfaa19d6e8acfebbf0d81e1b9ab2
matthew goodman <mattsgood2@aol.com>
2017-11-13 19:06:10 +0000

    rm templates in dirs

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'movies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')'templates',],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
