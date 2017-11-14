3a28d0d6150fe8c7870b1a85489c78a33f0e9f3c
matthew goodman <mattsgood2@aol.com>
2017-11-14 16:26:38 +0000

    db url rm

--- movies/movies/deploy_settings/__init__.py
+++ movies/movies/deploy_settings/__init__.py
@@ -12,5 +12,5 @@ ALLOWED_HOSTS = [
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
-db_from_env = dj_database_url.config('postgres://vygqqqdzifusjk:6f9e92348c2319fa66a17fa91df3fce43fc1ffd986b2c71aa372cd267b8ab92f@ec2-54-247-124-9.eu-west-1.compute.amazonaws.com:5432/ddf4m0tasds1qf')
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
