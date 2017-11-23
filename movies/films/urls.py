80ea73c26559374be7046d1e601e54656c1e7769
matthew goodman <mattsgood2@aol.com>
2017-11-23 11:58:31 +0000

    added class based Template views

--- movies/films/urls.py
+++ movies/films/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
 from . import views
+from .views import FilmListView
 
 urlpatterns = [
-    url(r'^$', views.film_list),
+    #url(r'^$', views.film_list),
+    url(r'^$', FilmListView.as_view(), name='Home'),
 
 ]
--- movies/films/views.py
+++ movies/films/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
-from django.views import View
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
 from .models import Film
 
 # Create your views here.
 
-def film_list(request):
-    films = Film.objects.all()
+#def film_list(request):
+    #films = Film.objects.all()
     #output = ','.join([str(film) for film in films])
-    return render(request, 'films/film_list.html', {'films': films})
+    #return render(request, 'films/film_list.html', {'films': films})
+
+class FilmListView(TemplateView):
+    template_name = 'films/film_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(FilmListView, self).get_context_data(**kwargs)
+        context['films'] = Film.objects.all()
+        return context
