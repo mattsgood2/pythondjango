ded614865003bb4b7e30902bbbaee18da73af46f
matthew goodman <mattsgood2@aol.com>
2017-11-27 17:31:02 +0000

    bootstrap and filmsdetail html

--- movies/films/admin.py
+++ movies/films/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import Film, FilmListDetail
 
 # Register your models here.
-admin.site.register(Film)
+
+class FilmListDetailInline(admin.StackedInline):
+    model = FilmListDetail
+
+
+class FilmAdmin(admin.ModelAdmin):
+    inlines = [
+        FilmListDetailInline,
+        ]
+
+
+admin.site.register(Film, FilmAdmin)
 admin.site.register(FilmListDetail)
